f333be85a6f779b168e16732630613b540b2f7b9
jakaria455173 <jakaria455173@gmail.com>
2023-06-07 02:20:17 +0600

    project use many npm package install

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import './style.css'
 import { RouterProvider } from 'react-router-dom'
-// import AuthProvider from './Providers/AuthProvider'
+import AuthProvider from './Providers/AuthProvider'
 import { router } from './Routes/Routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <AuthProvider>
+  <AuthProvider>
     <RouterProvider router={router} />
-  // {/* </AuthProvider> */}
+  </AuthProvider>
 )
