792a7ae0195d5feec344ffc6785845ca14fee2d0
jakaria455173 <jakaria455173@gmail.com>
2023-06-07 02:58:13 +0600

    simple common npm package setups

--- src/npm-package-import.js
+++ src/npm-package-import.js
@@ -17,6 +17,10 @@ import { Audio } from 'react-loader-spinner';
   wrapperClass
 />
 
+// React Sweet Alert-2
+
+
+
 //React Icons
 import { FaBeer } from 'react-icons/fa';
 
@@ -76,3 +80,40 @@ import { HelmetProvider } from 'react-helmet-async'
 
 // Initial Setup Axios
 
+import {useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import UseAuth from './UseAuth';
+
+const axiosSecure = axios.create({
+  baseURL: 'http://localhost:4000',
+});
+
+const useAxiosSecure = () => {
+  const {logOut}=UseAuth()
+  const navigate = useNavigate();
+  useEffect(() => {
+    axiosSecure.interceptors.request.use((config) => {
+      const token = localStorage.getItem('access-token');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+
+    axiosSecure.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+          await logOut();
+          navigate('/login');
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [logOut, navigate]);
+
+  return [axiosSecure];
+};
+
+export default useAxiosSecure;
