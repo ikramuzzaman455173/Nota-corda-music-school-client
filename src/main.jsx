0a691d16e5815ea5746d8006c6a3f1b59e5d6a23
jakaria455173 <jakaria455173@gmail.com>
2023-06-07 02:33:19 +0600

    React query or Dynmaic Page Title Create React Hetmet Initial Setup Completes

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import './style.css'
 import { RouterProvider } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import AuthProvider from './Providers/AuthProvider'
 import { router } from './Routes/Routes'
-
+import { QueryClientProvider, QueryClient } from 'react-query'
+// Create a client
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <RouterProvider router={router} />
-  </AuthProvider>
+  <HelmetProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+      </QueryClientProvider>
+    </AuthProvider>
+  </HelmetProvider>
 )
