89e2bfc458de1b2e40c25908859f204d116d0d07
jakaria455173 <jakaria455173@gmail.com>
2023-06-07 21:15:13 +0600

    Custom Auth User Api Creates

---
+++ src/CommonApi/AuthUserApi.js
@@ -0,0 +1,19 @@
+//save a user to database
+export const savedUser = user => {
+  const currentUser = {
+    email: user.email,
+    name: user.displayName,
+    role:'user'
+  }
+  fetch(`http://localhost:4000/users/${user?.email}`, {
+    method: 'PUT',
+    headers: {
+      'content-type':'application/json'
+    },
+    body:JSON.stringify(currentUser)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    }).catch(error=>console.log(`404 page not found ${error}`))
+}
