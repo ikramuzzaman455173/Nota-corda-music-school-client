0b638dbbcb22a509bafac0e408d91d555b52dd48
jakaria455173 <jakaria455173@gmail.com>
2023-06-09 02:47:04 +0600

    react awesome reveal animation add infoText components

--- src/Components/SharedComponents/InfoText.jsx
+++ src/Components/SharedComponents/InfoText.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { keyframes } from "@emotion/react";
+import { Fade } from "react-awesome-reveal";
+const InfoText = ({ title }) => {
+  const customAnimation = keyframes`
+  from {
+    opacity: 0;
+    transform: translate3d(200px, -100px, 0);
+  }
 
-const InfoText = ({title}) => {
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+`;
   return (
     <>
-      <h3 className='md:text-5xl sm:text-3xl dark:font-Merienda py-10 font-Pt text-3xl font-bold text-slate-700 dark:text-white text-center mb-10 underline decoration-double'>{title}</h3></>
+      <Fade cascade keyframes={customAnimation}>
+        <h3 className='md:text-5xl sm:text-3xl dark:font-Merienda py-10 font-Pt text-3xl font-bold text-slate-700 dark:text-white text-center mb-10 underline decoration-double'>{title}</h3>
+      </Fade>
+    </>
   )
 }
 
 export default InfoText
+
