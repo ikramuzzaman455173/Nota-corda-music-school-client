7f3ea05f6aa90c55e184ae2dcd88a4adcbf88d5b
jakaria455173 <jakaria455173@gmail.com>
2023-06-09 05:50:24 +0600

    student dashboard select class page style part completes

--- src/Pages/Dashboard/SelectClasses.jsx
+++ src/Pages/Dashboard/SelectClasses.jsx
@@ -4,67 +4,78 @@ const SelectClasses = () => {
   return (
     <>
       <h3 className='text-center my-10 font-bold tracking-wider text-slate-500 dark:text-white underline decoration-double md:text-3xl text-xl font-Pt dark:font-Merienda'>Total Selected Classes Are: <span className='text-info dark:text-warning'>0</span></h3>
-
-      <section className="antialiased bg-gray-100 text-gray-600 h-screen px-4">
-        <div className="flex flex-col justify-center h-full">
-          {/* Table */}
-          <div className="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
-            <header className="px-5 py-4 border-b border-gray-100">
-              <h2 className="font-semibold text-gray-800">Customers</h2>
-            </header>
-            <div className="p-3">
-              <div className="overflow-x-auto">
-                <table className="table-auto w-full">
-                  <thead className="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
-                    <tr>
-                      <th className="p-2 whitespace-nowrap">
-                        <div className="font-semibold text-left">Name</div>
-                      </th>
-                      <th className="p-2 whitespace-nowrap">
-                        <div className="font-semibold text-left">Email</div>
-                      </th>
-                      <th className="p-2 whitespace-nowrap">
-                        <div className="font-semibold text-left">Spent</div>
-                      </th>
-                      <th className="p-2 whitespace-nowrap">
-                        <div className="font-semibold text-center">Country</div>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="text-sm divide-y divide-gray-100">
-                    <tr>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="flex items-center">
-                          <div className="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
-                            <img
-                              className="rounded-full"
-                              src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg"
-                              width="40"
-                              height="40"
-                              alt="Alex Shatov"
-                            />
-                          </div>
-                          <div className="font-medium text-gray-800">Alex Shatov</div>
+      <div className="flex flex-col justify-center h-full">
+        {/* Table */}
+        <div className="w-full max-w-5xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200 dark:bg-gradient-to-r dark:from-[#010314] dark:to-[#0f0728]">
+          <div className="p-3">
+            <div className="overflow-x-auto">
+              <table className="table-auto w-full">
+                <thead className="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
+                  <tr>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-left">sl</div>
+                    </th>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-left">Class Bg</div>
+                    </th>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-left">Class Name</div>
+                    </th>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-left">price</div>
+                    </th>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-center">class lavel</div>
+                    </th>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-center">class duration</div>
+                    </th>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-center">action</div>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="text-sm divide-y dark:text-white divide-gray-100">
+                  <tr>
+                    <td className="p-2 whitespace-nowrap">
+                      <div className="text-left">1</div>
+                    </td>
+                    <td className="p-2 whitespace-nowrap">
+                      <div className="flex items-center">
+                        <div className="w-10 h-10 mr-2 sm:mr-3">
+                          <img
+                            className="rounded-md"
+                            src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg"
+                            width="40"
+                            height="40"
+                            alt="class img"
+                          />
                         </div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left">alexshatov@gmail.com</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-left font-medium text-green-500">$2,890.66</div>
-                      </td>
-                      <td className="p-2 whitespace-nowrap">
-                        <div className="text-lg text-center">??</div>
-                      </td>
-                    </tr>
-                    {/* Add more rows for other customers */}
-                  </tbody>
-                </table>
-              </div>
+                      </div>
+                    </td>
+                    <td className="p-2 whitespace-nowrap">
+                      <div className="text-left">alexshatov@gmail.com</div>
+                    </td>
+                    <td className="p-2 whitespace-nowrap">
+                      <div className="text-left font-medium text-info dark:text-warning">$2,890.66</div>
+                    </td>
+                    <td className="p-2 whitespace-nowrap">
+                      <div className="text-lg text-center">begginer</div>
+                    </td>
+                    <td className="p-2 whitespace-nowrap">
+                      <div className="text-lg text-center">1 hours</div>
+                    </td>
+                    <td className="p-2 whitespace-nowrap">
+                      <div className="text-lg text-center"><button className='awesome-btn px-10 py-[2px] rounded-full'>pay</button></div>
+                    </td>
+                  </tr>
+
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-      </section>
+      </div>
     </>
   )
 }
