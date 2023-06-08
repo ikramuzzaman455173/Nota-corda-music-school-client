3933fed9d4ea34ed0d60f9b5cb0d527b995b6f53
jakaria455173 <jakaria455173@gmail.com>
2023-06-09 02:14:12 +0600

    scroll to page top button add

--- src/Layouts/MainPage.jsx
+++ src/Layouts/MainPage.jsx
@@ -7,13 +7,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import Footer from "../Components/SharedComponents/Footer";
 import UseAuth from "../Hooks/UseAuth";
 import LoadingSpinner from "../Components/SharedComponents/LoadingSpinner";
-
+import ScrollToTopButton from "../Components/SharedComponents/ScrollToTopButton";
 const MainPage = () => {
   const { loading } = UseAuth()
   if (loading) {
     return(<LoadingSpinner/>)
   }
-  
+
   return (
     <div className="bg-white text-slate-600 dark:bg-gradient-to-r dark:from-[#010314] dark:to-[#0f0728]">
         <Helmet>
@@ -21,6 +21,7 @@ const MainPage = () => {
       </Helmet>
       <NavBar />
       <div className='min-h-[calc(100vh-68px)] pt-24 pb-10 '>
+        <ScrollToTopButton/>
       <Outlet></Outlet>
       </div>
       <Footer/>
